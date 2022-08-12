33e8275fb1e1c13b7d7689e28340a50b0a928060
Belik85 <beletskyidmytro@gmail.com>
2022-08-12 06:33:38 +0200

    add questionComponent

--- src/common/components/learnPageComponents/QuestionsForLearnPage.tsx
+++ src/common/components/learnPageComponents/QuestionsForLearnPage.tsx
@@ -6,7 +6,6 @@ export const QuestionsForLearnPage = () => {
     return (
         <div>
             <Wrapper>
-
                 <div className={style.container}>
                     <h1 className={style.learn}>Learn "Pack Name"</h1>
                     <h4 className={style.question}>Question: "How "This" works in JavaScript?"</h4>
